9e48405c143e052a5aba9dbeb2999b38e647f814
Lucas Costa <lucas_costa_95@hotmail.com>
2026-09-10 21:44:10 -0300

    chore: bump version to 1.2.0

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import java.security.MessageDigest
 
-val appVersion = "1.1.0"
+val appVersion = "1.2.0"
 val githubOwner = "lucascosta95"
 val githubRepo = "aws-local-manager"
 
